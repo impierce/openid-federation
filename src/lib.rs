55cd26acac12a226a83752d5ef5e7e0817495ba0
Nander Stabel <nander.stabel@impierce.com>
2025-10-02 16:03:23 +0200

    feat: initialize basic Rust project structure

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+//! # OpenID Federation
+//!
+//! A Rust implementation of the OpenID Federation 1.0 standard.
+//!
+//! This library provides support for OpenID Federation, which allows
+//! for the creation of trust relationships between OpenID Connect providers
+//! and relying parties through a federation of trust anchors.
+
+#![warn(missing_docs)]
+#![warn(clippy::all)]
+
